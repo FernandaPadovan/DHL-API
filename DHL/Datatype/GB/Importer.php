4f07340d8c0c238747fcfea7756df1f5f7a4f2a8
Aethalyste <benefactor09@gmail.com>
2022-01-10 15:05:57 +0200

    ANA-333 ShipmentRequest.Importer (#11)
    * ANA-333 ShipmentRequest.Importer
    
    * ANA-333 added DivisionCode
    
    * ANA-333 AddressLine1
    
    * ANA-333 remove DivisionCode
    
    Co-authored-by: Nikita Chernyak <nikita.chernyak@dataart.com>

---
+++ DHL/Datatype/GB/Importer.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace DHL\Datatype\GB;
+use DHL\Datatype\Base;
+
+class Importer extends Base
+{
+    /**
+     * Is this object a subobject
+     * @var boolean
+     */
+    protected $_isSubobject = true;
+
+    /**
+     * Parameters of the datatype
+     * @var array
+     */
+    protected $_params = array(
+        'CompanyName' => array(
+            'type' => 'CompanyNameValidator',
+            'required' => false,
+            'subobject' => false,
+            'comment' => 'Name of company / business',
+            'minLength' => '0',
+        ),
+        'AddressLine1' => array(
+            'type' => 'string',
+            'required' => true,
+            'subobject' => false,
+        ),
+        'AddressLine2' => array(
+            'type' => 'string',
+            'required' => false,
+            'subobject' => false,
+        ),
+        'City' => array(
+            'type' => 'City',
+            'required' => false,
+            'subobject' => false,
+            'comment' => 'City name',
+        ),
+        'PostalCode' => array(
+            'type' => 'PostalCode',
+            'required' => false,
+            'subobject' => false,
+            'comment' => 'Full postal/zip code for address',
+        ),
+        'CountryCode' => array(
+            'type' => 'CountryCode',
+            'required' => false,
+            'subobject' => false,
+            'comment' => 'ISO country codes',
+            'length' => '2',
+        ),
+        'CountryName' => array(
+            'type' => 'CountryName',
+            'required' => false,
+            'subobject' => false,
+            'comment' => 'ISO country name',
+        ),
+        'Contact' => array(
+            'type' => 'Contact',
+            'required' => false,
+            'subobject' => true,
+        ),
+        'RegistrationNumbers' => array(
+            'type' => 'RegistrationNumber',
+            'required' => false,
+            'subobject' => true,
+            'multivalues' => true,
+        ),
+    );
+}
